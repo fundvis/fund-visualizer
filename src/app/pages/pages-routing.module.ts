219a88884896d3300a1484c2160b2249aad002eb
duanqizhi <duanqizhi@meizu.com>
2018-08-29 16:09:58 +0800

    Remove useless dashboard component

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,17 +2,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { PagesComponent } from './pages.component';
-import { ECommerceComponent } from './e-commerce/e-commerce.component';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
 import { RankComponent } from './rank/rank.component';
 import { DetailComponent } from './detail/detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [{
   path: '',
   component: PagesComponent,
   children: [{
     path: 'dashboard',
-    component: ECommerceComponent,
+    component: DashboardComponent,
   }, {
     path: 'rank',
     component: RankComponent,
