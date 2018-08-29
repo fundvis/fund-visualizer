3fbc7ba7cfac0bf4d6fb7956ffcf1fdda8b29fee
duanqz <duanqz@gmail.com>
2018-08-29 15:48:29 +0800

    Add fund detail

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -6,13 +6,58 @@ import { GithubService } from '../../github.service';
     templateUrl: 'detail.component.html',
 })
 export class DetailComponent implements AfterViewInit {
+
+    options: any = {};
+
     constructor(private github: GithubService) {
     }
 
     ngAfterViewInit(): void {
         this.github.readFile('000057.json').then(data => {
-            console.info(data);
+            const netvalues = data[0]['netvalues'];
+            if (netvalues) {
+                this.setOptions(netvalues);
+            }
         });
+    }
+
+    setOptions(data) {
+        const xAxisData = [];
+        const yAxisData = [];
+
+        for (let i = data.length - 1; i >= 0; i--) {
+            xAxisData.push(data[i][0]);
+            yAxisData.push(data[i][1]);
+        }
+
+        this.options = {
+            legend: {
+                data: ['bar'],
+                align: 'left',
+            },
+            tooltip: {},
+            xAxis: {
+                data: xAxisData,
+                silent: false,
+                splitLine: {
+                    show: false,
+                },
+            },
+            yAxis: {
+            },
+            series: [{
+                name: '单位净值',
+                type: 'bar',
+                data: yAxisData,
+                animationDelay: function (idx) {
+                    return idx * 10;
+                },
+            }],
+            animationEasing: 'elasticOut',
+            animationDelayUpdate: function (idx) {
+                return idx * 5;
+            },
+        };
     }
 
     onSearch(event): void {
--- src/app/pages/detail/detail.module.ts
+++ src/app/pages/detail/detail.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { DetailComponent } from './detail.component';
 import { ThemeModule } from '../../@theme/theme.module';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 @NgModule({
   declarations: [DetailComponent],
   imports: [
     ThemeModule,
+    NgxEchartsModule,
   ],
   providers: [
   ],
