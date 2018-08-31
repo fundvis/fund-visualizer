714202fa4c36df50f8732fcf68c0d9ff97867b18
duanqz <duanqz@gmail.com>
2018-08-31 17:33:48 +0800

    Remove useless dashboard scss

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -8,7 +8,6 @@ import { GrowthRateViewComponent } from '../widget/fund-table-viewcell/growthrat
 
 @Component({
   selector: 'ngx-dashboard',
-  styleUrls: ['./dashboard.component.scss'],
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements AfterViewInit {
